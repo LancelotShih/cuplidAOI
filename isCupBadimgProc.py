78d937a83ae882523060d179db57cfe0c8e27696
LancelotShih <lancelot.shih@gmail.com>
2023-05-30 13:25:18 +0800

    prepped for svm on warning label

--- isCupBadimgProc.py
+++ isCupBadimgProc.py
@@ -5,30 +5,16 @@ import numpy as np
 import cv2
 import os
 
-imagesG = os.listdir(r'cupInspectAOI\good')
-# imagesB = os.listdir(r'cupInspectAOI\bad')
+# imagesG = os.listdir(r'C:\Users\user\Documents\GitHub\cuplidAOI\processedCupImages\goodProc')
+imagesB = os.listdir(r'C:\Users\user\Documents\GitHub\cuplidAOI\processedCupImages\badProc')
+template = cv2.imread(r"processedCupImages\warningTemplate.png", 0)
+h, w = template.shape
 
 count = 0
 
-for i in imagesG:
-    img = cv2.imread(r"cupInspectAOI\good\\" + i)
-    img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (7,7), 0)
-
-    (T, threshInv) = cv2.threshold(blurred,130,255, cv2.THRESH_BINARY)
-    cv2.imshow("detected?", threshInv)
-    masked = cv2.bitwise_and(img, img, mask = threshInv)
-    cv2.imshow("clipped", masked)
-    status = cv2.imwrite("cupInspectAOI\goodProc\procimg" + str(count) + ".png", masked)
-    print("written to disk", status)
-    count+=1
-    cv2.waitKey(0)
-
-# for i in imagesB:
-#     img = cv2.imread(r"cupInspectAOI\bad\\" + i)
-#     img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
+# for i in imagesG:
+#     img = cv2.imread(r"C:\Users\user\Documents\GitHub\cuplidAOI\processedCupImages\goodProc\\" + i)
+#     # img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
 
 #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #     blurred = cv2.GaussianBlur(gray, (7,7), 0)
@@ -37,7 +23,53 @@ for i in imagesG:
 #     cv2.imshow("detected?", threshInv)
 #     masked = cv2.bitwise_and(img, img, mask = threshInv)
 #     cv2.imshow("clipped", masked)
-#     status = cv2.imwrite(r"cupInspectAOI\badProc\procimg" + str(count) + ".png", masked)
-#     print("written to disk", status)
+#     # status = cv2.imwrite("cupInspectAOI\goodProc\procimg" + str(count) + ".png", masked)
+#     # print("written to disk", status)
+    
+
+#     # template match the warning
+#     print("Finding label...")
+#     img2 = gray.copy()
+#     result = cv2.matchTemplate(img2, template, cv2.TM_CCORR_NORMED)
+#     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+#     location = max_loc
+#     bottomRight = (location[0] + w, location[1] + h)
+#     print(location)
+#     img2 = img2[location[1]: location[1] + h , location[0]: location[0] + w]
+#     cv2.rectangle(img2, location, bottomRight, 255, 5)
+#     cv2.imshow('Match', img2)
+#     cv2.imwrite("processedCupImages\goodCrop\gCrop" + str(count) + ".png", img2)
 #     count+=1
 #     cv2.waitKey(0)
+
+for i in imagesB:
+    img = cv2.imread(r"C:\Users\user\Documents\GitHub\cuplidAOI\processedCupImages\badProc\\" + i)
+    # img = cv2.resize(img, (0,0), fx = 0.5, fy = 0.5)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (7,7), 0)
+
+    (T, threshInv) = cv2.threshold(blurred,130,255, cv2.THRESH_BINARY)
+    cv2.imshow("detected?", threshInv)
+    masked = cv2.bitwise_and(img, img, mask = threshInv)
+    cv2.imshow("clipped", masked)
+    status = cv2.imwrite(r"processedCupImages\badCrop\bCrop" + str(count) + ".png", masked)
+    print("written to disk", status)
+
+# template matching 
+    print("Finding label...")
+    img2 = gray.copy()
+    result = cv2.matchTemplate(img2, template, cv2.TM_CCORR_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    location = max_loc
+    bottomRight = (location[0] + w, location[1] + h)
+    print(location)
+    cv2.rectangle(img2, location, bottomRight, 255, 5)
+    cv2.imshow('Match', img2)
+    img2 = img2[location[1]: location[1] + h , location[0]: location[0] + w]
+    cv2.rectangle(img2, location, bottomRight, 255, 5)
+    cv2.imshow('Match', img2)
+    cv2.imwrite(r"processedCupImages\badCrop\bCrop" + str(count) + ".png", img2)
+
+    count+=1
+    cv2.waitKey(0)
--- isCupBadsvm.py
+++ isCupBadsvm.py
@@ -8,63 +8,66 @@ import random
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-# dir = r'C:\Users\lance\OneDrive\Documents\College yr2\OView Code\cupInspectAOI\processedCup'
-
-# categories = ['badProc', 'goodProc']
-
-# data = []
-
-# for category in categories:
-#     path = os.path.join(dir, category)
-#     label = categories.index(category)
-
-#     for img in os.listdir(path):
-#         imgpath = os.path.join(path,img)
-#         goodbad_img = cv2.imread(imgpath,0)
-#         try:
-#             goodbad_img = cv2.resize(goodbad_img,(50,50))
-#             image = np.array(goodbad_img).flatten()
-
-#             data.append([image,label])
-#         except Exception as e:
-#             pass
-
-# print(len(data))
-# pick_in = open('data1.pickle', 'wb')
-# pickle.dump(data, pick_in)
-# pick_in.close()
-
-pick_in = open('cupInspectAOI\data1.pickle', 'rb')
-data = pickle.load(pick_in)
-pick_in.close()
-
-random.shuffle(data)
-features = []
-labels = []
-
-for feature, label in data:
-    features.append(feature)
-    labels.append(label)
-
-xtrain, xtest, ytrain, ytest = train_test_split(features, labels, test_size=0.9)
-
-# model = SVC(C = 1,kernel = 'linear', gamma = 'auto')
-# model.fit(xtrain, ytrain)
-
-pick = open('cupInspectAOI\model.sav', 'rb')
-# pickle.dump(model, pick)
-model = pickle.load(pick)
-pick.close()
-
-prediction = model.predict(xtest)
-accuracy = model.score(xtest, ytest)
+dir = r'C:\Users\user\Documents\GitHub\cuplidAOI\processedCupImages'
 
 categories = ['badProc', 'goodProc']
 
-print("Accuracy: ", accuracy)
+data = []
 
-print('Predition is : ', categories[prediction[0]])
+for category in categories:
+    path = os.path.join(dir, category)
+    label = categories.index(category)
 
-mycup = xtest[0].reshape(50, 50)
-plt.imshow(mycup, cmap='gray')
-plt.waitforbuttonpress()
+    for img in os.listdir(path):
+        imgpath = os.path.join(path,img)
+        goodbad_img = cv2.imread(imgpath,0)
+        try:
+            goodbad_img = cv2.resize(goodbad_img,(540,540))
+            image = np.array(goodbad_img).flatten()
+
+            data.append([image,label])
+        except Exception as e:
+            pass
+
+print(len(data))
+pick_in = open('data1.pickle', 'wb')
+pickle.dump(data, pick_in)
+pick_in.close()
+
+# pick_in = open('data1.pickle', 'rb')
+# data = pickle.load(pick_in)
+# pick_in.close()
+
+# random.shuffle(data)
+# features = []
+# labels = []
+
+
+# model application starts below
+
+# for feature, label in data:
+#     features.append(feature)
+#     labels.append(label)
+
+# xtrain, xtest, ytrain, ytest = train_test_split(features, labels, test_size=0.9)
+
+# # model = SVC(C = 1,kernel = 'linear', gamma = 'auto')
+# # model.fit(xtrain, ytrain)
+
+# pick = open('model.sav', 'rb')
+# # pickle.dump(model, pick)
+# model = pickle.load(pick)
+# pick.close()
+
+# prediction = model.predict(xtest)
+# accuracy = model.score(xtest, ytest)
+
+# categories = ['badProc', 'goodProc']
+
+# print("Accuracy: ", accuracy)
+
+# print('Predition is : ', categories[prediction[0]])
+
+# mycup = xtest[0].reshape(50, 50)
+# plt.imshow(mycup, cmap='gray')
+# plt.waitforbuttonpress()
